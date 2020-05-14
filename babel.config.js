b543ace87d2701df2d3a31c78e9ed4b3e77c5f69
lanc <bluelanc@gmail.com>
2020-05-14 14:32:21 +0800

    add remove console setting

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+const plugins = [
+  "@babel/syntax-dynamic-import"
+];
+
+if (IS_PROD) { // 拔 console
+  plugins.push("transform-remove-console");
+}
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  plugins: [
-    [
-      '@babel/syntax-dynamic-import'
-    ]
-  ]
+  plugins: plugins
 };
